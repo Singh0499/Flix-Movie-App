735bb2da84e8057c0111c032002d570eddd0a15a
Gurpreet Singh <gurpreetsingh0499@gmail.com>
2023-06-20 18:55:50 +0530

    Page Router Link

--- js/apikey.js
+++ js/apikey.js
@@ -1,1 +1,4 @@
 const apiKey='2e632688db41064e6bb5935290e59764';
+const apiURL = 'https://api.themoviedb.org/3/';
+
+export{apiKey,apiURL};
--- js/script.js
+++ js/script.js
@@ -1,10 +1,24 @@
+import { apiKey, apiURL } from "./apikey.js";
+
 const global={
     currentPath:window.location.pathname,
 };
 // Fetch Data From TMBD API
 
+async function displayPopularMovies(){
+    const result = await fetchDataFromAPI('movie/popular')
+    console.log(result);
+}
+
+
 async function fetchDataFromAPI(endpoint){
-    pass   
+    const api_key = apiKey;
+    const api_URL = apiURL;
+
+    const response = await fetch(`${api_URL}${endpoint}?api_key=${api_key}&language=en-US`);
+
+    const data =await response.json();
+    return data;
 }
 fetchDataFromAPI();
 // Init App
@@ -13,22 +27,22 @@ function init(){
     
     switch (global.currentPath){
         case  '/':
-            case '/index.html':
-                console.log('Home');
-                break;
-                case '/shows.html':
-                    console.log('Shows');
-                    break;
-                    case '/movie-details.html':
-                        console.log('Movie Details');
-                        break;
-                        case '/tv-details.html':
-                            console.log('TV Details');
-                            break;
-                            case '/search.html':
-                                console.log('Search');
-                                break;
-                            }
+        case '/index.html':
+            displayPopularMovies();
+            break;
+        case '/shows.html':
+            console.log('Shows');
+            break;
+        case '/movie-details.html':
+            console.log('Movie Details');
+            break;
+        case '/tv-details.html':
+            console.log('TV Details');
+            break;
+        case '/search.html':
+            console.log('Search');
+            break;
+    }
                             
 }
 
