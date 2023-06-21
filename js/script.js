2ea44686c4ae12f92da0d64184003c5e4897a797
Gurpreet Singh <gurpreetsingh0499@gmail.com>
2023-06-21 20:30:26 +0530

    Display Popular movies

--- js/script.js
+++ js/script.js
@@ -6,8 +6,32 @@ const global={
 // Fetch Data From TMBD API
 
 async function displayPopularMovies(){
-    const result = await fetchDataFromAPI('movie/popular')
-    console.log(result);
+    const { results } = await fetchDataFromAPI('movie/popular')
+    results.forEach(movie=> {
+        const div= document.createElement('div');
+        div.classList.add('card');
+        div.innerHTML = `
+        <a href="movie-details.html?id=${movie.id}">
+          ${movie.poster_path?`<img
+            src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+            class="card-img-top"
+            alt="${movie.title}"
+            />`:`<img
+          src="images/no-image.jpg"
+          class="card-img-top"
+          alt="${movie.title}"
+        />`}
+        </a>
+        <div class="card-body">
+          <h5 class="card-title">${movie.title}</h5>
+          <p class="card-text">
+            <small class="text-muted">Release: ${movie.release_date}</small>
+          </p>
+        </div>`;
+
+        document.querySelector('#popular-movies').appendChild(div);
+    });
+
 }
 
 
