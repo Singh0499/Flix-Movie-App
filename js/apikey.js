5898eb1216256baf491f0981fdb2a60e3a9cc1b3
Gurpreet Singh <gurpreetsingh0499@gmail.com>
2023-06-21 21:16:09 +0530

    Spinner and TV shows added

--- js/apikey.js
+++ js/apikey.js
@@ -1,4 +1,3 @@
+export{apiKey,apiURL};
 const apiKey='2e632688db41064e6bb5935290e59764';
 const apiURL = 'https://api.themoviedb.org/3/';
-
-export{apiKey,apiURL};
--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ const global={
 
 async function displayPopularMovies(){
     const { results } = await fetchDataFromAPI('movie/popular')
-    results.forEach(movie=> {
+    results.forEach((movie)=> {
         const div= document.createElement('div');
         div.classList.add('card');
         div.innerHTML = `
@@ -35,17 +35,70 @@ async function displayPopularMovies(){
 }
 
 
+async function displayPopularTVShows(){
+    const { results } = await fetchDataFromAPI('tv/popular')
+    results.forEach((show)=> {
+        const div= document.createElement('div');
+        div.classList.add('card');
+        div.innerHTML = `
+        <a href="tv-details.html?id=${show.id}">
+          ${show.poster_path?`<img
+            src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+            class="card-img-top"
+            alt="${show.name}"
+            />`:`<img
+          src="images/no-image.jpg"
+          class="card-img-top"
+          alt="${show.name}"
+        />`}
+        </a>
+        <div class="card-body">
+          <h5 class="card-title">${show.name}</h5>
+          <p class="card-text">
+            <small class="text-muted">Air Date: ${show.first_air_date}</small>
+          </p>
+        </div>`;
+
+        document.querySelector('#popular-shows').appendChild(div);
+    });
+
+}
+
+
+
 async function fetchDataFromAPI(endpoint){
     const api_key = apiKey;
     const api_URL = apiURL;
 
-    const response = await fetch(`${api_URL}${endpoint}?api_key=${api_key}&language=en-US`);
+    showSpinner();
 
+    const response = await fetch(`${api_URL}${endpoint}?api_key=${api_key}&language=en-US`);
     const data =await response.json();
+
+    hideSpinner();
+
     return data;
 }
+
+
+function showSpinner(){
+    document.querySelector('.spinner').classList.add('show');
+}
+function hideSpinner(){
+    document.querySelector('.spinner').classList.remove('show');
+}
+
+
+
+
+
+
 fetchDataFromAPI();
 // Init App
+
+
+
+
 
 function init(){
     
@@ -55,7 +108,7 @@ function init(){
             displayPopularMovies();
             break;
         case '/shows.html':
-            console.log('Shows');
+            displayPopularTVShows();
             break;
         case '/movie-details.html':
             console.log('Movie Details');
